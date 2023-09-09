d4b91c9d750e357b5ffd9b4b83abe53e7aae592c
marv0659 <marv0659@stud.kea.dk>
2023-09-09 23:18:22 +0200

    added færdighe opgave og ny knap og nyt design

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const taskList = document.getElementById("taskList");
 const taskStringInput = document.getElementById("taskString");
 const quantityInput = document.getElementById("quantity");
 const undoButton = document.querySelector("button");
+const addTaskBttn = document.querySelector("#addTaskbtn");
+const undoBttn = document.querySelector("#undoTaskbtn");
+
+addTaskBttn.addEventListener("click",addTask);
+undoBttn.addEventListener("click", undoDeletion);
 
 let tasks = [];
 let deletedTasks = [];
+let completedTasks = [];
 
 function addTask() {
     const taskString = taskStringInput.value;
@@ -37,6 +43,21 @@ function undoDeletion() {
             }
         }
 
+        function markAsDone(taskId) {
+            const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+            if (taskIndex !== -1) {
+                const completedTask = tasks.splice(taskIndex, 1)[0];
+                completedTasks.push(completedTask);
+                renderTasks();
+                renderCompletedTasks();
+            }
+        }
+
+
+
+        
+
         function clearInputs() {
             taskStringInput.value = "";
             quantityInput.value = "";
@@ -46,11 +67,24 @@ function undoDeletion() {
             taskList.innerHTML = "";
             tasks.forEach(task => {
                 const li = document.createElement("li");
-                li.textContent = `ID: ${task.id}, Task: ${task.taskString}, Quantity: ${task.quantity}`;
+                li.textContent = `( ${task.id} ), ${task.taskString} | ${task.quantity}`;
                 const deleteButton = document.createElement("button");
+                const doneButton = document.createElement("button");
                 deleteButton.textContent = "Delete";
+                doneButton.textContent = "Done";
+                doneButton.onclick = () => markAsDone(task.id);
                 deleteButton.onclick = () => deleteTask(task.id);
                 li.appendChild(deleteButton);
+                li.appendChild(doneButton);
                 taskList.appendChild(li);
+            });
+        }
+
+        function renderCompletedTasks() {
+            completedTaskList.innerHTML = "";
+            completedTasks.forEach(task => {
+                const li = document.createElement("li");
+                li.textContent = `( ${task.id} ), ${task.taskString} | ${task.quantity}`;
+                completedTaskList.appendChild(li);
             });
         }
